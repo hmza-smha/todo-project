96e7b7b7bac9ea56a79fcff94f010bc8722b568e
HamZa Akram Samha <smha.hmza@gmail.com>
2022-02-10 15:12:47 +0200

    AddScripts

---
+++ scripts/app.js
@@ -0,0 +1,24 @@
+var userName = prompt("Inter your name");
+var userGender = prompt("Inter your Gender (Male/Female)").toLocaleLowerCase();
+
+while(userGender != "male" && userGender != "female"){
+    userGender = prompt("Inter your Gender (Male/Female)");
+}
+
+var userAge = prompt("Inter your Age");
+while(userAge <= 0){
+    alert("Your age can not be less than or equal to zero.");
+    userAge = prompt("Inter your Age");
+}
+
+var welcomeMsg = confirm("Do you want to see welcome message?");
+if(welcomeMsg){
+
+    if(userGender == "male"){
+        alert("Hello Mr. " + userName)
+    }
+    else{
+        alert("Hello Ms. " + userName)
+    }
+    
+}
